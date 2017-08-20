41917651f4b3dc7290eb5c2e46c6fbd5d6251258
Maful P Arnandi <mafulprayoga@gmail.com>
2017-08-20 08:47:20 +0700

    Show book

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get books_index_url
+  test "should get show" do
+    get books_show_url
     assert_response :success
   end
 
